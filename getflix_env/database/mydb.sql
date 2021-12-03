bd3380cf4aad5c3c93c2b426324ccc4ecbcfdd78
Gysen Lionel <gysenlionel@gmail.com>
2021-12-03 15:05:29 +0100

    create table

--- getflix_env/database/mydb.sql
+++ getflix_env/database/mydb.sql
@@ -11,3 +11,19 @@ CREATE TABLE register (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
+INSERT INTO `register` (`id`, `first_name`, `last_name`, `email`, `password`, `user_type`) VALUES
+(1, 'lionel', 'hello123', 'test@gmail.com', '12345678a', 'admin'),
+(13, 'clara', 'doe', 'clara@gmail.com', '1234578a', 'admin'),
+(12, 'david', 'dupond', 'david@gmail.com', '12345678a', 'admin'),
+(14, 'morgane', 'dupond', 'morgane@gmail.com', '12345678a', 'admin'),
+(14, 'user', 'dupond', 'user@gmail.com', '12345678a', 'user');
+
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `id_movie` varchar(250) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
